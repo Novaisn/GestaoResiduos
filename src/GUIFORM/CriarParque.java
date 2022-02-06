a78f137512aff69622c938ff3b215b7be53c9021
Nelson <novaisnelson@ipvc.pt>
2022-02-06 16:53:13 +0000

    Commit final acho

--- src/GUIFORM/CriarParque.java
+++ src/GUIFORM/CriarParque.java
@@ -30,10 +30,10 @@ public class CriarParque {
         repo = Repositorio.getRepositorio();
         List<Contentor> contentores = new ArrayList<>();
         for (Contentor c : repo.getContentorMap().values()){
-            if ( c.getMunicipio().getIdMunicipio() == municipio.getIdMunicipio()) {
-                if (c.getParqueContentores() == null) {
+            if ( c.getMunicipio().getIdMunicipio() == municipio.getIdMunicipio() && c.getParqueContentores() == null) {
+
                     comboBoxContentor.addItem(c);
-                }
+
             }
         }
 
